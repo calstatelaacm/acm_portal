075ba50a2c51e96bdb359ffe3b695bce1b8b164c
Srivats Venkataraman <srivats.venkataraman@gmail.com>
2020-10-28 14:09:23 -0700

    Changed the size of the text field on the login and create account page. Also added two new dependencies

--- lib/Authentication/CreateAccount/CreateAccount.dart
+++ lib/Authentication/CreateAccount/CreateAccount.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:universal_platform/universal_platform.dart';
 
 class CreateAccount extends StatefulWidget {
   static const String route = '/signup';
@@ -13,6 +14,7 @@ class CreateAccount extends StatefulWidget {
 class _CreateAccountState extends State<CreateAccount> {
   final GlobalKey<FormState> _createAccount = new GlobalKey<FormState>();
   TextEditingController email, pwd, name, classStanding, major;
+  var mobileSize, webSize;
 
   @override
   void initState() {
@@ -27,6 +29,16 @@ class _CreateAccountState extends State<CreateAccount> {
 
   @override
   Widget build(BuildContext context) {
+    mobileSize = (MediaQuery.of(context).size.width*9)/(10);
+    webSize = MediaQuery.of(context).size.width/2;
+
+    if(UniversalPlatform.isAndroid || MediaQuery.of(context).size.width < 600){
+      return SafeArea(
+        child: Scaffold(
+          body: mobileForm(),
+        )
+      );
+    }
     return SafeArea(
       child: Scaffold(
         body: createAccount(),
@@ -34,7 +46,7 @@ class _CreateAccountState extends State<CreateAccount> {
     );
   }
 
-  Widget createAccount(){
+  Widget mobileForm(){
     return Container(
       decoration: BoxDecoration(
           image: DecorationImage(
@@ -50,7 +62,7 @@ class _CreateAccountState extends State<CreateAccount> {
               children: [
                 Image.asset('assets/acmlogo1.png', height: 100,),
                 SizedBox(
-                  width: MediaQuery.of(context).size.width/2,
+                  width: mobileSize,
                   child: TextFormField(
                     controller: name,
                     decoration: InputDecoration(
@@ -78,7 +90,7 @@ class _CreateAccountState extends State<CreateAccount> {
                 ),
                 SizedBox(height: 20,),
                 SizedBox(
-                  width: MediaQuery.of(context).size.width/2,
+                  width: mobileSize,
                   child: TextFormField(
                     controller: email,
                     decoration: InputDecoration(
@@ -106,7 +118,176 @@ class _CreateAccountState extends State<CreateAccount> {
                 ),
                 SizedBox(height: 20,),
                 SizedBox(
-                  width: MediaQuery.of(context).size.width/2,
+                  width: mobileSize,
+                  child: TextFormField(
+                    controller: pwd,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      hintText: 'Must be 6+ characters',
+                      filled: true,
+                      // fillColor: Colors.white.withOpacity(.85),
+                      labelStyle: GoogleFonts.openSans(color: Colors.black, fontSize: 20),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(5)),
+                          borderSide: BorderSide(
+                              color: Colors.white
+                          )
+                      ),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.all(
+                              Radius.circular(5)),
+                          borderSide: BorderSide(
+                              color: Colors.white
+                          )
+                      ),
+                    ),
+                    obscureText: true,
+                  ),
+                ),
+                SizedBox(height: 20,),
+                SizedBox(
+                  width: mobileSize,
+                  child: TextFormField(
+                    controller: major,
+                    decoration: InputDecoration(
+                        labelText: 'Major',
+                        filled: true,
+                        // fillColor: Colors.white.withOpacity(.85),
+                        labelStyle: GoogleFonts.openSans(color: Colors.black, fontSize: 20),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(5)),
+                            borderSide: BorderSide(
+                                color: Colors.white
+                            )
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(5)),
+                            borderSide: BorderSide(
+                                color: Colors.white
+                            )
+                        )
+
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                SizedBox(
+                  width: mobileSize,
+                  child: TextFormField(
+                    controller: classStanding,
+                    decoration: InputDecoration(
+                        labelText: 'Class Standing',
+                        filled: true,
+                        // fillColor: Colors.white.withOpacity(.85),
+                        labelStyle: GoogleFonts.openSans(color: Colors.black, fontSize: 20),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(5)),
+                            borderSide: BorderSide(
+                                color: Colors.white
+                            )
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(5)),
+                            borderSide: BorderSide(
+                                color: Colors.white
+                            )
+                        )
+
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                RaisedButton(
+                  onPressed: submitForm,
+                  child: Text("Create Account", style: GoogleFonts.openSans(),),
+                  color: Colors.blue,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget createAccount(){
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/sideshow1.png'),
+              fit: BoxFit.cover
+          )
+      ),
+      child: Center(
+        child: SingleChildScrollView(
+          child: Form(
+            key: _createAccount,
+            child: Column(
+              children: [
+                Image.asset('assets/acmlogo1.png', height: 100,),
+                SizedBox(
+                  width: webSize,
+                  child: TextFormField(
+                    controller: name,
+                    decoration: InputDecoration(
+                        labelText: 'Name',
+                        filled: true,
+                        // fillColor: Colors.white.withOpacity(.85),
+                        labelStyle: GoogleFonts.openSans(color: Colors.black, fontSize: 20),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(5)),
+                            borderSide: BorderSide(
+                                color: Colors.white
+                            )
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(5)),
+                            borderSide: BorderSide(
+                                color: Colors.white
+                            )
+                        )
+
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                SizedBox(
+                  width: webSize,
+                  child: TextFormField(
+                    controller: email,
+                    decoration: InputDecoration(
+                        labelText: 'email',
+                        filled: true,
+                        // fillColor: Colors.white.withOpacity(.85),
+                        labelStyle: GoogleFonts.openSans(color: Colors.black, fontSize: 20),
+                        focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(5)),
+                            borderSide: BorderSide(
+                                color: Colors.white
+                            )
+                        ),
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(
+                                Radius.circular(5)),
+                            borderSide: BorderSide(
+                                color: Colors.white
+                            )
+                        )
+
+                    ),
+                  ),
+                ),
+                SizedBox(height: 20,),
+                SizedBox(
+                  width: webSize,
                   child: TextFormField(
                     controller: pwd,
                     decoration: InputDecoration(
@@ -135,7 +316,7 @@ class _CreateAccountState extends State<CreateAccount> {
                 ),
                 SizedBox(height: 20,),
                 SizedBox(
-                  width: MediaQuery.of(context).size.width/2,
+                  width: webSize,
                   child: TextFormField(
                     controller: major,
                     decoration: InputDecoration(
@@ -163,7 +344,7 @@ class _CreateAccountState extends State<CreateAccount> {
                 ),
                 SizedBox(height: 20,),
                 SizedBox(
-                  width: MediaQuery.of(context).size.width/2,
+                  width: webSize,
                   child: TextFormField(
                     controller: classStanding,
                     decoration: InputDecoration(
--- lib/Authentication/Login/login.dart
+++ lib/Authentication/Login/login.dart
@@ -1,10 +1,7 @@
-
-import 'package:acm_web/Authentication/CreateAccount/CreateAccount.dart';
-import 'package:acm_web/Screens/Events/Events.dart';
-
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:universal_platform/universal_platform.dart';
 
 class Login extends StatefulWidget {
   static const String route = '/login';
@@ -15,17 +12,28 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
   String email, pwd;
+  var mobileSize, webSize;
 
   @override
   Widget build(BuildContext context) {
+    mobileSize = (MediaQuery.of(context).size.width*9)/10;
+    webSize = MediaQuery.of(context).size.width/2;
+
+    if(UniversalPlatform.isAndroid || MediaQuery.of(context).size.width < 600){
+      return SafeArea(
+          child: Scaffold(
+            body: mobile(),
+          )
+      );
+    }
     return SafeArea(
         child: Scaffold(
-          body: loginForm(),
+          body: web(),
         )
     );
   }
 
-  Widget loginForm(){
+  Widget mobile(){
     return Container(
       decoration: BoxDecoration(
           image: DecorationImage(
@@ -45,7 +53,7 @@ class _LoginState extends State<Login> {
               child: Column(
                 children: [
                   SizedBox(
-                    width: MediaQuery.of(context).size.width/2,
+                    width: mobileSize,
                     child: TextFormField(
                       validator: (input){
                         if(input.isEmpty){
@@ -80,7 +88,117 @@ class _LoginState extends State<Login> {
                   ),
                   SizedBox(height: 20,),
                   SizedBox(
-                    width: MediaQuery.of(context).size.width/2,
+                    width: mobileSize,
+                    child: TextFormField(
+                      validator: (input){
+                        if(input.isEmpty){
+                          return 'Password is required';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                          labelText: 'Password',
+                          filled: true,
+                          labelStyle: GoogleFonts.openSans(color: Colors.black, fontSize: 20),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(5)),
+                              borderSide: BorderSide(
+                                  color: Colors.white
+                              )
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(5)))),
+                      onSaved: (input) => pwd = input,
+                      obscureText: true,
+                    ),
+                  ),
+                  SizedBox(height: 10,),
+                  RaisedButton(
+                    onPressed: signIn,
+                    child: Text("Login", style: GoogleFonts.openSans(),),
+                  ),
+                  SizedBox(height: 10,),
+                  Divider(
+                    indent: 20,
+                    endIndent: 20,
+                    color: Colors.white,
+                    thickness: 1,
+                  ),
+                  SizedBox(height: 10,),
+                  GestureDetector(
+                    onTap: (){
+                      Navigator.of(context).pushNamed('/signup');
+                    },
+                    child: Text("New? Create Account", textAlign: TextAlign.center,
+                      style: GoogleFonts.openSans(fontSize: 20),),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget web(){
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('assets/sideshow1.png'),
+              fit: BoxFit.cover
+          )
+      ),
+      child: Center(
+        child: ListView(
+          physics: BouncingScrollPhysics(),
+          shrinkWrap: true,
+          children: [
+            Image.asset('assets/acmlogo1.png', height: 100,),
+            SizedBox(height: 10,),
+            Form(
+              key: _formKey,
+              child: Column(
+                children: [
+                  SizedBox(
+                    width: webSize,
+                    child: TextFormField(
+                      validator: (input){
+                        if(input.isEmpty){
+                          return 'Email is required';
+                        }
+                        return null;
+                      },
+                      decoration: InputDecoration(
+                          labelText: 'Email',
+                          filled: true,
+                          // fillColor: Colors.white.withOpacity(.85),
+                          labelStyle: GoogleFonts.openSans(color: Colors.black, fontSize: 20),
+                          focusedBorder: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(5)),
+                              borderSide: BorderSide(
+                                  color: Colors.white
+                              )
+                          ),
+                          border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(5)),
+                              borderSide: BorderSide(
+                                  color: Colors.white
+                              )
+                          )
+
+                      ),
+                      onSaved: (input) => email = input,
+                      keyboardType: TextInputType.emailAddress,
+                    ),
+                  ),
+                  SizedBox(height: 20,),
+                  SizedBox(
+                    width: webSize,
                     child: TextFormField(
                       validator: (input){
                         if(input.isEmpty){
